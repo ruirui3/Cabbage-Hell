a8c59c35b7a657187f725a83808a4a1642079cf8
Toxarcher <114118447+Toxarcher@users.noreply.github.com>
2024-10-22 16:28:57 -0700

    added player movement
    added player movement to sprite, but for some reason when the game runs, the code works, but the sprite doesn't show.

--- Babbage Final/Assets/Scripts/PlayerController.cs
+++ Babbage Final/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+
+    private float horizontal;
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        horizontal *= speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(horizontal, 0f, 0f));
+
+
+
     }
 }
