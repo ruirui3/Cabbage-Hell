1d11e616d6fd43b4a3cb68b0f1b650a26b35fc21
Sophie Yu <sophie.20.yu@gmail.com>
2024-10-27 17:45:14 -0700

    Update BulletScript.cs
    changed so that bullet disappears on contact w enemy (anything) or stops moving when out of screen

--- Babbage Final/Assets/Scripts/BulletScript.cs
+++ Babbage Final/Assets/Scripts/BulletScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public float speed = 5f;
+    public bool isMoving = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,24 @@ public class BulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isMoving){ 
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (notInScreen()){
+            isMoving = false;
+        }
+
+    }
+    private bool notInScreen(){
+        Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position); //set variable to cameras position
+        //^ Viewport position in Unity refers to the coordinates of a point in relation to the camera's viewport
+        return viewportPosition.y > 1; //1 is top of the screen
+    }
+    private void whenCollide(Collision2D collision) {
+        gopoof();
+    }
+    private void gopoof() {
+        // Disable the GameObject
+        gameObject.SetActive(false);
     }
 }
