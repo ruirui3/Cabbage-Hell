2567fb5b75df13e0667ded4582d0a88387487875
Harry <oyjunhan@hotmail.com>
2024-11-10 20:20:19 -0800

    Implement repeating background
    TODO: make image repeatability more seamless

---
+++ Babbage Final/Assets/Sprites/background/RepeatBG.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBG : MonoBehaviour
+{
+    Vector3 startPos;
+    float repeatHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position; // remember starting pos as current position
+        repeatHeight = gameObject.GetComponent<Renderer>().bounds.size.y / 2; // repeat for half of image height
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if out of screen, go back to start pos
+        if (transform.position.y < startPos.y - repeatHeight) {
+            transform.position = startPos;
+        }
+    }
+}
