e0917c7063a4368f4c36cc1a1d48d450e970009c
Sophie Yu <sophie.20.yu@gmail.com>
2024-12-06 14:43:13 -0800

    New MainMenu Scene implementation
    play button on mainmenu scene, when click play runs the mainscene

--- Babbage Final/Assets/MainMenu.cs
+++ Babbage Final/Assets/MainMenu.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MainMenu : MonoBehaviour
 {
     public void Play(){
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync("MainScene");
     }
 }
