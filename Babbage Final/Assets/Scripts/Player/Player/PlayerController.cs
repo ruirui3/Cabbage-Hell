0996ca3593377e9bda8b64a59dca8e2cabc52393
ruirui3 <ruiruithing@gmail.com>
2025-02-21 18:36:08 -0800

    Revamped entire PlayerController interface to fit queue based bullets

--- Babbage Final/Assets/Scripts/Player/Player/PlayerController.cs
+++ Babbage Final/Assets/Scripts/Player/Player/PlayerController.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public GameObject bullet;
     
-    public float FIRING_DELAY_MUTIPLIER = 1f;
+    public float FIRING_DELAY_MUTIPLIER = 1f; //could consider having multiple firing delay multiplier constants for each respective bullet types
     public float SPEED_MULTIPLIER = 1f;
     private float horizontal;
     public float bulletFiringDelay; // seconds per bullets
     public float ms = 0f;
     private float leftBound = -3f;
     private float rightBound = 3f;
-    private bool tripleBullets = false;
 
     private float acceleration = 10f;
     private float deceleration = 20f;
@@ -21,19 +21,103 @@ public class PlayerController : MonoBehaviour
     private float currentVelocity = 0f;
     private bool reversing = false; // If the key pressed is opposing motion
 
+    private int currentBulletType;
+    private Queue<int> bulletQueue;
+
     void Start()
     {
-        tripleBullets = true;
-        bulletFiringDelay = 1f; 
+        bulletFiringDelay = 1f;
+        bulletQueue = new Queue<int>(); //need some way to reference bullets in inventory. Current bullet types: normal (0) triple (1) carrot (2) honey (3) curl (4) tornado (5)     - mostly in terms of progression of implementation
+        
+        for (int i = 0; i < 6; i++)
+        {
+            bulletQueue.Enqueue(i);  //Temporary for loop for enqueuing all of the bullet types
+        }
+
     }
 
     void Update()
     {
+
+        currentBulletType = bulletQueue.Peek();
+        UpdatePosition();
+        SeekAndRunBulletType(currentBulletType);
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            tripleBullets = !tripleBullets;
+            SwitchBulletType();
         }
 
+    }
+
+    public void SwitchBulletType()
+    {
+        if (bulletQueue.Count != 0)
+        {
+            bulletQueue.Enqueue(bulletQueue.Dequeue()); //puts item in front to back, and places 2nd item to the front
+        }
+    }
+
+    private void SeekAndRunBulletType(int bulletIndex)
+    {
+        ms += Time.deltaTime;
+
+        if (bulletIndex == 0) //normal
+        {
+            NormalBulletFunction();
+        }
+        if (bulletIndex == 1) //triple
+        {
+            TripleBulletFunction();
+        }
+        if (bulletIndex == 2) //carrot
+        {
+            Debug.Log("Currently attempting to run carrot bullet. Not implemented. Please swap bullet type.");
+            ms = 0;
+        }
+        if (bulletIndex == 3) //honey
+        {
+            Debug.Log("Currently attempting to run honey bullet. Not implemented. Please swap bullet type.");
+            ms = 0;
+        }
+        if (bulletIndex == 4) //curl
+        {
+            Debug.Log("Currently attempting to run curl bullet. Not implemented. Please swap bullet type.");
+            ms = 0;
+        }
+        if (bulletIndex == 5) //tornado
+        {
+            Debug.Log("Currently attempting to run tornado bullet. Not implemented. Please swap bullet type.");
+            ms = 0;
+        }
+    }
+
+    private void NormalBulletFunction()
+    {
+        if (ms >= bulletFiringDelay * FIRING_DELAY_MUTIPLIER)
+        {
+            Vector3 spawnPosition = transform.position + Vector3.up * 1.5f;
+            Instantiate(bullet, spawnPosition, transform.rotation);
+
+            ms = 0;
+        }
+    }
+
+    private void TripleBulletFunction()
+    {
+        if (ms >= bulletFiringDelay * FIRING_DELAY_MUTIPLIER)
+        {
+            Vector3 spawnPosition = transform.position + Vector3.up * 1.5f;
+            Instantiate(bullet, spawnPosition, transform.rotation);
+            Instantiate(bullet, spawnPosition, Quaternion.Euler(0, 0, 45));
+            Instantiate(bullet, spawnPosition, Quaternion.Euler(0, 0, 315));
+
+            ms = 0;
+        }
+    }
+
+    private void UpdatePosition()
+    {
         float moveInput = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
@@ -89,8 +173,8 @@ public class PlayerController : MonoBehaviour
         transform.position += new Vector3(currentVelocity * Time.deltaTime, 0, 0) * SPEED_MULTIPLIER;
 
         transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, 
-            leftBound, rightBound), transform.position.y, 
+            Mathf.Clamp(transform.position.x,
+            leftBound, rightBound), transform.position.y,
             transform.position.z
             ); //checks if in bound
 
@@ -100,20 +184,5 @@ public class PlayerController : MonoBehaviour
             currentVelocity = 0f;
         }
 
-        ms += Time.deltaTime;
-
-        if (ms >= bulletFiringDelay * FIRING_DELAY_MUTIPLIER)
-        {
-            Vector3 spawnPosition = transform.position + Vector3.up * 1.5f;
-            Instantiate(bullet, spawnPosition, transform.rotation);
-
-            if (tripleBullets)
-            {
-                Instantiate(bullet, spawnPosition, Quaternion.Euler(0, 0, 45));
-                Instantiate(bullet, spawnPosition, Quaternion.Euler(0, 0, 315));
-            }
-
-            ms = 0;
-        }
     }
 }
