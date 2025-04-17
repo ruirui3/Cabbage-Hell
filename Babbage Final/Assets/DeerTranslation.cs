0469f665eaeef65a86f5baf89adc8d2466e925c3
Sophie Yu <sophie.20.yu@gmail.com>
2025-04-16 21:31:28 -0700

    finally deer show up

--- Babbage Final/Assets/DeerTranslation.cs
+++ Babbage Final/Assets/DeerTranslation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DeerTranslation : MonoBehaviour
 {
+    float speed = 2.5f;
+    public float bottomBound = -5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class DeerTranslation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.down * Time.deltaTime * speed);
+        if (transform.position.y < bottomBound){
+            goPoof();
+        }
+    }
+    void goPoof() {
+        Destroy(gameObject);
     }
 }
