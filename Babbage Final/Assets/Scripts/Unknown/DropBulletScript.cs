8d21eed8cd1cf68816278a644b82b820c73d478c
Sophie Yu <sophie.20.yu@gmail.com>
2025-06-06 20:51:32 -0700

    changed bullet drop rates

--- Babbage Final/Assets/Scripts/Unknown/DropBulletScript.cs
+++ Babbage Final/Assets/Scripts/Unknown/DropBulletScript.cs
@@ -6,7 +6,7 @@ public class DropBulletScript : MonoBehaviour
 {   
     public PlayerController shooter;
     public AudioClip bulletDropSound;
-    float[] bulletChance = {0f, 0.2f, 0.5f, 0f, 0.1f, 0f}; // normal (0) triple (1) carrot (2) honey (3) curl (4) tornado (5)
+    float[] bulletChance = {0f, 0.3f, 0.5f, 0f, 0.25f, 0f}; // normal (0) triple (1) carrot (2) honey (3) curl (4) tornado (5)
     int[] bulletStackCount = {0, 5, 10, 5, 10, 5};
 
     // Start is called before the first frame update
