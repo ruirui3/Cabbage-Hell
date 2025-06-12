204581c602fadb350e4dfcddaedba87f9e120405
Toxarcher <114118447+Toxarcher@users.noreply.github.com>
2025-06-11 22:38:41 -0700

    attempted to add death sound
    i added the code to play the death sound on death but notjhing is working. I tried using AI and still nothing works

--- Babbage Final/Assets/Scripts/GUI/ManageHealth.cs
+++ Babbage Final/Assets/Scripts/GUI/ManageHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,13 @@ public class ManageHealth : MonoBehaviour
     public Image healthBar;
     public float healthAmount = 100f;
     private float maxHealth;
-
+    public AudioClip deathSFX; // assign in Inspector
+    private Boolean isDead;
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = healthAmount;
+        isDead = false;
     }
 
     // Update is called once per frame
@@ -25,7 +28,7 @@ public class ManageHealth : MonoBehaviour
         {
             TakeDamage(20);
         }
-
+ 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Heal(5);
@@ -37,31 +40,40 @@ public class ManageHealth : MonoBehaviour
     public void TakeDamage(float damage)
     {
 
-        if (healthAmount-damage <=0)
+        if (healthAmount - damage <= 0 && isDead == false)
         {
             healthAmount = 0; //maybe trigger game end
-        } else
+            AudioSource.PlayClipAtPoint(deathSFX, transform.position);
+            isDead = true;
+        }
+        else
         {
             healthAmount -= damage;
         }
 
-        
+
         healthBar.fillAmount = healthAmount / 100f;
     }
 
     public void Heal(float heal)
     {
 
-        if (healthAmount+heal > maxHealth)
+        if (healthAmount + heal > maxHealth)
         {
             healthAmount = maxHealth;
-        } else
+        }
+        else
         {
             healthAmount += heal;
         }
 
         healthAmount = Mathf.Clamp(healthAmount, 0, 100);
         healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public float GetHealth()
+    {
+        return healthAmount;
     }
 
 }
